9d85523298827cd1a78e46d32cf5d633c53823c5
shotgun45 <jipeles@yahoo.com>
2025-08-09 23:26:07 -0400

    chapter 1 lesson 5

--- main.go
+++ main.go
@@ -17,7 +17,15 @@ func main() {
 		input := scanner.Text()
 		words := cleanInput(input)
 		if len(words) > 0 {
-			fmt.Printf("Your command was: %s\n", strings.ToLower(words[0]))
+			cmdName := strings.ToLower(words[0])
+			if cmd, ok := commandRegistry[cmdName]; ok {
+				err := cmd.callback()
+				if err != nil {
+					fmt.Printf("Error: %v\n", err)
+				}
+			} else {
+				fmt.Println("Unknown command:", cmdName)
+			}
 		}
 	}
 }
@@ -25,4 +33,38 @@ func main() {
 func cleanInput(text string) []string {
 	fields := strings.Fields(strings.TrimSpace(text))
 	return fields
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commandRegistry = map[string]cliCommand{
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
 }
